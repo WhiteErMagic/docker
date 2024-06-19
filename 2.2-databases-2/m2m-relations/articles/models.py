761360a1c4057eaa987ff3b2c190f06ddb480012
Boris <kabv@mail.ru>
2024-06-19 10:26:40 +0600

    revision

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -18,7 +18,8 @@ class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
+    tag = models.ManyToManyField(Tag, through='Scopeship')
 
     class Meta:
         verbose_name = 'Статья'
