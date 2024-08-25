b050ed6a6451a8ba08fc6ec53ef12c0591e209de
Boris <kabv@mail.ru>
2024-08-25 11:48:56 +0600

    Initial commit

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import SensorViewCreate, MeasurementViewCreate, SensorViewUpdate, SensorViewGet
-
-urlpatterns = [
-    path('sensors/', SensorViewCreate.as_view()),
-    path('add/', MeasurementViewCreate.as_view()),
-    path('sensors/<int:pk>', SensorViewUpdate.as_view()),
-    path('sensor/<int:pk>', SensorViewGet.as_view()),
-]
